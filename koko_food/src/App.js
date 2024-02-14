1874110641901c536f30db3e504f7f314cd06f31
shubhammm14 <shubhamshaw905@gmail.com>
2024-02-14 15:17:56 +0530

    reset modules commit

--- koko_food/src/App.js
+++ koko_food/src/App.js
@@ -1,9 +1,15 @@
-
+import {Routes,Route} from 'react-router-dom'
+import Navbar from './Component/Navbar';
 
 function App() {
   return (
     <div>
-      
+      <div className=''>
+        <Navbar/>
+      </div>
+      <div>
+
+      </div>
     </div>
   );
 }
